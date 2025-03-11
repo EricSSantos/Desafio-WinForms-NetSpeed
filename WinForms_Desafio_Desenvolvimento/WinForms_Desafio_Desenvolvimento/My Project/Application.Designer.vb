67b4331800693d24d2f54eeba5d9991fb5f7f16b
EricSSantos <ericsilva333@outlook.com>
2025-03-11 14:06:54 -0300

    - Ajuste no formulário de inicialização: alterado para frmPrincipal, em vez de Form1, que não existia.
    - Reorganização da estrutura de pastas para melhor organização e manutenção

--- WinForms_Desafio_Desenvolvimento/WinForms_Desafio_Desenvolvimento/My Project/Application.Designer.vb
+++ WinForms_Desafio_Desenvolvimento/WinForms_Desafio_Desenvolvimento/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WinForms_Desafio_Desenvolvimento.Form1
+            Me.MainForm = Global.WinForms_Desafio_Desenvolvimento.frmPrincipal
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
